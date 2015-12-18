13c3ca1ad8bc0f0f19b84d71a7b062d639c4fdd6
David Gudeman <davidmgudeman@gmail.com>
2015-12-17 20:49:45 -0800

    It finds the appropriate tabale file and can open it

--- src/AccessDB.java
+++ src/AccessDB.java
@@ -1,8 +1,39 @@
+import java.io.BufferedReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
+import java.util.StringTokenizer;
 
 public class AccessDB
 {
 
+    public static Path makeAPath()
+    {
+        Path file = null;
+        try {
+          file = Paths.get("M:\\Q_FILES\\Q_DATABASES\\Test.mdb");
+//            BufferedReader reader = Files.newBufferedReader(file, Charset.defaultCharset());
+//            
+//            String line;
+//            while((line = reader.readLine()) != null) 
+//            {
+//                System.out.println(line);
+//                StringTokenizer tokenizer = new StringTokenizer(line,  " ");
+//                while((line = reader.readLine()) != null)
+//                {
+//                    System.out.println(line);
+//                }
+//                
+//            }
+//            reader.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return file;
+    }
     public static void main(String[] args)
     {
        Connection con;
@@ -10,12 +41,13 @@ public class AccessDB
        ResultSet re;
        try 
        {
-          // Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-           con = DriverManager.getConnection("jdbc:ucanaccess://HyperLink.mdb");
+        
+       //    con = DriverManager.getConnection("jdbc:ucanaccess://HyperLink.mdb");
+           con = DriverManager.getConnection("jdbc:ucanaccess://" + makeAPath());
            st = con.createStatement();
-           re=st.executeQuery("SELECT * FROM tblMain");
+           re=st.executeQuery("SELECT * FROM tblOne");
            while(re.next()) {
-               System.out.println(re.getInt(1) + re.getString(2) + re.getString(3) + " " + re.getString(4));
+               System.out.println(re.getInt(1) + re.getString(2));
            }
        } catch (Exception e) {
            System.out.println(e);
